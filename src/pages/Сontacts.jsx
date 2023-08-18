bb553bae1b97d8021855b93d9983355a53f76635
SerhiiShapovalov <ShapovalovSV83@gmail.com>
2023-08-18 19:19:56 +0300

    Sixth commit

--- src/pages/Сontacts.jsx
+++ src/pages/Сontacts.jsx
@@ -2,7 +2,10 @@ import Filter from '../components/Filter/Filter';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Helmet } from 'react-helmet';
-import { getContacts, selectLoading } from '../redux/contacts/selectors';
+import {
+  getContacts,
+  // , selectLoading
+} from '../redux/contacts/selectors';
 import ContactList from '../components/ContactList/ContactList';
 import { fetchContacts } from '../redux/contacts/operations';
 import ContactForm from '../components/ContactForm/ContactForm';
@@ -10,7 +13,7 @@ import ContactForm from '../components/ContactForm/ContactForm';
 export default function ContactsPage() {
   const dispatch = useDispatch();
   const contacts = useSelector(getContacts);
-  const isLoading = useSelector(selectLoading);
+  // const isLoading = useSelector(selectLoading);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -22,7 +25,7 @@ export default function ContactsPage() {
         <title>Your contacts</title>
       </Helmet>
       <ContactForm />
-      <div>{isLoading && 'Request in progress...'}</div>
+      {/* <div>{isLoading && 'Request in progress...'}</div> */}
       <Filter />
       {contacts.length > 0 && <ContactList />}
     </>
