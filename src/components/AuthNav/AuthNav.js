f6d4727494951cf1f3e25ddc19230dae05bebe55
SerhiiShapovalov <ShapovalovSV83@gmail.com>
2023-08-27 00:34:03 +0300

    Added .active for links

--- src/components/AuthNav/AuthNav.js
+++ src/components/AuthNav/AuthNav.js
@@ -1,13 +1,25 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import css from './AuthNav.module.css';
 
 export const AuthNav = () => {
+  const location = useLocation();
+
   return (
     <div>
-      <NavLink className={css.link} to="/register">
+      <NavLink
+        className={`${css.link} ${
+          location.pathname === '/register' ? css.active : ''
+        }`}
+        to="/register"
+      >
         Register
       </NavLink>
-      <NavLink className={css.link} to="/login">
+      <NavLink
+        className={`${css.link} ${
+          location.pathname === '/login' ? css.active : ''
+        }`}
+        to="/login"
+      >
         Log In
       </NavLink>
     </div>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,17 +1,26 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
 import css from './Navigation.module.css';
 
 export const Navigation = () => {
   const { isLoggedIn } = useAuth();
+  const location = useLocation();
 
   return (
     <nav>
-      <NavLink className={css.link} to="/">
+      <NavLink
+        className={`${css.link} ${location.pathname === '/' ? css.active : ''}`}
+        to="/"
+      >
         Home
       </NavLink>
       {isLoggedIn && (
-        <NavLink className={css.link} to="/contacts">
+        <NavLink
+          className={`${css.link} ${
+            location.pathname === '/contacts' ? css.active : ''
+          }`}
+          to="/contacts"
+        >
           Contacts
         </NavLink>
       )}
